4112ff0c2c66c4953192ac71da0d4f78591a8729
Kseniya Ovinnikova <studos@list.ru>
2024-05-26 19:05:40 +0500

    add program code and new file .gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace newFold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
